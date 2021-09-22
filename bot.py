0829f2ac5aa1e135f6ee60a8f259f1955ade51d2
Mimiview <axelgiu99@gmail.com>
2021-09-22 13:39:35 +0200

    added gitignore and hid the token KEY

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from music_cog import music_cog as music
 from discord.ext import commands
 import pafy 
+from dotenv import load_dotenv
+import os
 
 bot = commands.Bot(command_prefix='-')
 
@@ -9,8 +11,9 @@ bot = commands.Bot(command_prefix='-')
 bot.remove_command('help')
 
 bot.add_cog(music(bot))
+ 
+ 
+load_dotenv('.env')
 
-
-
-bot.run('ODgyNzE0NTQ4MTgyODU5ODU4.YS_Z_A.X_HPPBVo9SFPA4OFhUzWMphHH5g')
+bot.run(os.getenv('BOT_TOKEN'))
 
