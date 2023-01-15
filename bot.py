e66482033738f06999cf470083c7dfbe2a07cacd
Mimiview <57045037+Mimiview@users.noreply.github.com>
2023-01-15 18:36:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,25 @@
 import discord 
+import asyncio
 from music_cog import music_cog as music
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='-', intents = intents)
-  
-bot.remove_command('help')
-bot.load_extension('music_cog')
- 
+
+
 load_dotenv('.env')
 
-@bot.event
-async def on_ready():
-    print("Bot is Running...")
+async def main():
+    bot = commands.Bot(command_prefix='-', intents = intents)
+    bot.remove_command('help')
+    async with bot:
+        await bot.load_extension('music_cog')
+        await bot.start(os.getenv('BOT_TOKEN'))
+    @bot.event
+    async def on_ready():
+        print("Bot is Running...")
 
-bot.run(os.getenv('BOT_TOKEN'));
+
+
+asyncio.run(main());
